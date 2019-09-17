c7ad75b8e8cb9ab8a7301453d214b71fb7e9de91
Lup Gabriel <gwolf2u.website@gmail.com>
2019-09-17 18:49:05 +0300

    sm8150-common: import DeviceSettings
    sm8150-common: DNM sepolicy fuckery
    sm8150-common: KeyHandler: reduce log spam
    sm8150-common: sepolicy: Fix SEPolicy denials for DisplayMode
    
    Co-authored-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Change-Id: I4ce05ac1670eea8bced8595da27903903de427db
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- common.mk
+++ common.mk
@@ -224,6 +224,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.7 \
     vendor.qti.hardware.display.allocator@1.0-service
 
+#Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -415,7 +419,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
