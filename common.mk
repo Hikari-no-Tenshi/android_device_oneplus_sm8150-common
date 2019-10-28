7b6a29faecc3384e14d816b09d141636fec3cf4f
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-10-28 09:10:15 +0200

    sm8150-common: Decommonize all camera blobs

--- common.mk
+++ common.mk
@@ -153,15 +153,6 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 PRODUCT_PACKAGES += \
     libbthost_if
 
-# Camera
-PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libxml2 \
-    Snap \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor
-
 # CNE
 PRODUCT_PACKAGES += \
     cneapiclient \
