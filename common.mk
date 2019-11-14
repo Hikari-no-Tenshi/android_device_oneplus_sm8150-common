da77d8a12f5096cce9e8ef987478cbdc231e7619
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-11-14 10:31:00 +0200

    sm8150-common: Decommonize tri-state-key calibration daemon

--- common.mk
+++ common.mk
@@ -409,8 +409,7 @@ PRODUCT_PACKAGES += \
 # tri-state key
 PRODUCT_PACKAGES += \
     KeyHandler \
-    tri-state-key_daemon \
-    tri-state-key-calibrate_daemon
+    tri-state-key_daemon
 
 # Trust HAL
 PRODUCT_PACKAGES += \
